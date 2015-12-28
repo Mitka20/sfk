1f728d9905598d41c66ee9ba01acea454bc26802
Mitka <mitka20@gmail.com>
2015-12-28 13:07:59 +0200

    new

---
+++ lib/sfk.rb
@@ -0,0 +1,17 @@
+require 'sfk/version'
+
+module Sfk
+  # Your code goes here...
+  class << self
+
+    private
+
+    def file
+
+    end
+
+    def execute
+
+    end
+  end
+end
---
+++ lib/sfk/version.rb
@@ -0,0 +1,3 @@
+module Sfk
+  VERSION = "0.1.0"
+end
